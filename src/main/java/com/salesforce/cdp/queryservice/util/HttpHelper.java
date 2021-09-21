a008f6318670cd1a9c789946b2843a7b8a34b997
sonal-aggarwal <sonal.aggarwal@salesforce.com>
2021-09-21 15:46:27 +0530

    @W-9686740: objectmapper changes reverted as tableau version mismatches

--- src/main/java/com/salesforce/cdp/queryservice/util/HttpHelper.java
+++ src/main/java/com/salesforce/cdp/queryservice/util/HttpHelper.java
@@ -18,7 +18,6 @@ package com.salesforce.cdp.queryservice.util;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.Request;
@@ -33,14 +32,12 @@ import java.util.Map;
 @Slf4j
 public class HttpHelper {
 
-    private static final ObjectReader mapperReader = new ObjectMapper().reader();
-
     public static void handleErrorResponse(Response response, String propertyName) throws IOException {
         handleErrorResponse(response.body().string(), propertyName);
     }
 
     public static void handleErrorResponse(String response, String propertyName) throws IOException {
-        ObjectNode node = mapperReader.readValue(response, ObjectNode.class);
+        ObjectNode node = new ObjectMapper().readValue(response, ObjectNode.class);
         JsonNode jsonNode = node.get(propertyName);
         String message = jsonNode == null ?
                 String.format(Locale.ROOT, "Property %s is not defined", propertyName) :
@@ -58,7 +55,7 @@ public class HttpHelper {
     }
 
     public static <T> T handleSuccessResponse(String responseString, Class<T> type) throws IOException {
-        return mapperReader.readValue(responseString, type);
+        return new ObjectMapper().readValue(responseString, type);
     }
 
     protected static Request buildRequest(String method, String url, RequestBody body, Map<String, String> headers) {
