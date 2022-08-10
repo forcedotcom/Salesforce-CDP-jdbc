bc5e450ac405b150f78cf35e167e27fa7c0e07b2
psasikaladevi <psasikaladevi@salesforce.com>
2022-08-10 10:58:53 +0530

    Update ExtractArrowUtil.java

--- src/main/java/com/salesforce/cdp/queryservice/util/ExtractArrowUtil.java
+++ src/main/java/com/salesforce/cdp/queryservice/util/ExtractArrowUtil.java
@@ -54,76 +54,7 @@ public class ExtractArrowUtil extends  ArrowUtil {
 		}
 	}
 
-	/**
-	 * Converts the arrow stream in to List<Map<String,Object>> so that it can then be converted into result set format.
-	 * @param queryServiceResponse Response received from query service
-	 * @return List of data map.
-	 * @throws SQLException
-	 */
-	public List<Object> getResultSetDataFromArrowStream(QueryServiceResponse queryServiceResponse, boolean isCursorBasedPaginationReq) throws SQLException {
-
-		byte[] bytes = Base64.getDecoder().decode(queryServiceResponse.getArrowStream());
-		ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
-		List<FieldVector> fieldVectors = null;
-
-		RootAllocator allocator = new RootAllocator(Long.MAX_VALUE);
-		List<Object> data = new ArrayList<>();
-		try (ArrowStreamReader arrowStreamReader = new ArrowStreamReader(inputStream, allocator)) {
-
-			VectorSchemaRoot root = arrowStreamReader.getVectorSchemaRoot();
-			fieldVectors = root.getFieldVectors();
-			while (arrowStreamReader.loadNextBatch()) {
-				int rowCount = fieldVectors.get(0).getValueCount();
-				for(int i=0;i<rowCount;++i) {
-
-					if(isCursorBasedPaginationReq) {
-						List<Object> row = new ArrayList<>();
-						for(FieldVector fieldVector : fieldVectors) {
-							Object fieldValue = this.getFieldValue(fieldVector,i);
-							row.add(fieldValue);
-						}
-						data.add(row);
-					} else {
-						Map<String,Object> row = new HashMap<>();
-						for(FieldVector fieldVector : fieldVectors) {
-							String fieldName = fieldVector.getField().getName();
-							Object fieldValue = this.getFieldValue(fieldVector,i);
-							row.put(fieldName,fieldValue);
-						}
-						data.add(row);
-					}
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw new SQLException("Failed to parse the arrow stream", e);
-		}
-		finally {
-			if(inputStream != null) {
-				try {
-					inputStream.close();
-				}
-				catch (IOException ex) {
-					ex.printStackTrace();
-				}
-				inputStream = null;
-			}
-			if(fieldVectors != null){
-				for(FieldVector fieldVector : fieldVectors) {
-					if(fieldVector !=  null) {
-						fieldVector.close();
-					}
-				}
-				fieldVectors = null;
-			}
-			allocator.close();
-			allocator =  null;
-		}
-		return  data;
-	}
-
-
-    public void initialiseArrowReaderForStream(InputStream inputStream){
+	public void initialiseArrowReaderForStream(InputStream inputStream){
 		if(arrowStreamReader == null){
 			 streamRootAllocator = new RootAllocator(Long.MAX_VALUE);
 			arrowStreamReader =new ArrowStreamReader(inputStream, streamRootAllocator );
